c0064cd006aee856b7e3999da8e40b27d81fcb47
merabza <merabza@gmail.com>
2023-08-23 11:51:51 +0400

    202308231151

--- CarcassContracts/ErrorModels/CommonErrors.cs
+++ CarcassContracts/ErrorModels/CommonErrors.cs
@@ -7,6 +7,8 @@ public static class CommonErrors
     public static readonly Err IncorrectData =
         new() { ErrorCode = nameof(IncorrectData), ErrorMessage = "არასწორი მონაცემები" };
 
-    public static Err CannotFindMethod(string methodName) =>
-        new() { ErrorCode = nameof(CannotFindMethod), ErrorMessage = "cannot find method {methodName}" };
+    public static Err CannotFindMethod(string methodName)
+    {
+        return new Err { ErrorCode = nameof(CannotFindMethod), ErrorMessage = "cannot find method {methodName}" };
+    }
 }
