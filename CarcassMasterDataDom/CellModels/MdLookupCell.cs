7d5cdba3977c29977668e84f1bfc6a0ef9f54c04
merabza <merabza@gmail.com>
2023-11-30 16:33:39 +0400

    202311301633

---
+++ CarcassMasterDataDom/CellModels/MdLookupCell.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace CarcassMasterDataDom.CellModels;
+
+public sealed class MdLookupCell : IntegerCell
+{
+
+    public MdLookupCell(string fieldName, string? caption, string dtTable,
+        string? intErrCode = null, string? intErrMessage = null, bool visible = true, string? typeName = null) : base(
+        fieldName, caption, intErrCode, intErrMessage, visible, typeName ?? "MdLookup")
+    {
+        DtTable = dtTable;
+    }
+
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public string? DtTable { get; set; }
+
+    public static MdLookupCell Create(string fieldName, string? caption, string dtTable, string? intErrCode = null,
+        string? intErrMessage = null, bool visible = true, string? typeName = null)
+    {
+        return new MdLookupCell(fieldName, caption, dtTable, intErrCode, intErrMessage, visible, typeName);
+    }
+
+    public new MdLookupCell Default(int defaultValue = default)
+    {
+        base.Default(defaultValue);
+        return this;
+    }
+}
