8239041411579318a24539953d1ef5ef3f689716
merabza <merabza@gmail.com>
2026-01-04 23:33:17 +0400

    202601042333

--- CarcassApplication/Services/Authentication/RegistrationService.cs
+++ CarcassApplication/Services/Authentication/RegistrationService.cs
@@ -21,19 +21,11 @@ public class RegistrationService : LoginBase, IScopeServiceCarcassApplication
     public async Task<OneOf<LoginResult, Err[]>> TryToRegister(RegisterParameters registerParameters,
         CancellationToken cancellationToken = default)
     {
-        //if (string.IsNullOrWhiteSpace(request.UserName))
-        //    return await Task.FromResult(new[]
-        //        { CarcassApiErrors.IsEmpty(nameof(request.UserName), "მომხმარებლის სახელი") });
-
         //მოწოდებული მომხმარებლის სახელით ხომ არ არსებობს უკვე რომელიმე მომხმარებელი
         var user = await UserMgr.FindByNameAsync(registerParameters.UserName);
         //თუ მოიძებნა ასეთი, დავაბრუნოთ შეცდომა
         if (user != null)
             return new[] { AuthenticationApiErrors.UserAlreadyExists };
-
-        //if (request.Email == null)
-        //    return await Task.FromResult(new[]
-        //        { CarcassApiErrors.IsEmpty(nameof(request.Email), "ელექტრონული ფოსტის მისამართი") });
 
         //მოწოდებული მომხმარებლის სახელით ხომ არ არსებობს უკვე რომელიმე მომხმარებელი
         user = await UserMgr.FindByEmailAsync(registerParameters.Email);
@@ -52,33 +44,5 @@ public class RegistrationService : LoginBase, IScopeServiceCarcassApplication
             return new[] { AuthenticationApiErrors.MoreComplexPasswordIsRequired };
 
         return await LoginProcess(user, registerParameters.Password, cancellationToken);
-
-        ////აქ მოვალთ მხოლოდ იმ შემთხვევაში, თუ მომხმარებელი წარმატებით შეიქმნა,
-        ////მოხდეს მისი ავტომატური ავტორიზაცია
-        //user = await DoLogin(_signinMgr, user, registerParameters.Password);
-        //if (user == null)
-        //    return new[] { AuthenticationApiErrors.CouldNotCreateNewUser };
-
-        //if (user.UserName == null)
-        //    return new[] { AuthenticationApiErrors.InvalidUsername };
-
-        //if (user.Email == null)
-        //    return new[] { AuthenticationApiErrors.InvalidEmail };
-
-        ////ახლადშექმნილ მომხმარებელს როლები არ აქვს, ამიტომ შემდეგი ბრძანება დაკომენტარებულია
-        ////თუ მომავალში საჭირო გახდა, რომ ახლადშექმნილ მომხმარებელს ავტომატურად მიეცეს როლი, მაშინ შემდეგი ბრძანება უნდა აღსდგეს
-        ////IList<string> roles = _userMgr.GetRolesAsync(user).Result;
-
-        //if (_identitySettings.Value.JwtSecret is null)
-        //    return new[] { CarcassApiErrors.ParametersAreInvalid };
-
-        //var token = user.CreateJwToken(_identitySettings.Value.JwtSecret, LastSequentialNumber);
-
-        //if (token is null)
-        //    return new[] { AuthenticationApiErrors.UsernameOrPasswordIsIncorrect };
-
-        //LastSequentialNumber++;
-
-        //return new LoginResult { User = user, LastSequentialNumber = LastSequentialNumber, Token = token };
     }
 }
