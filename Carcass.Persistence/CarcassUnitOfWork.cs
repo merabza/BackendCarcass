6d6e4a7cf969b7e287706417f7990d783f63e758
merabza <merabza@gmail.com>
2026-01-10 19:41:35 +0400

    202601101941

--- Carcass.Persistence/CarcassUnitOfWork.cs
+++ Carcass.Persistence/CarcassUnitOfWork.cs
@@ -1,57 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Carcass.Database;
-using DomainShared.Repositories;
-using LanguageExt;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
-using SystemToolsShared.Errors;
+﻿using Carcass.Database;
+using DomainShared;
 
 namespace Carcass.Persistence;
 
-public /*open*/ class CarcassUnitOfWork : IUnitOfWork
-{
-    private readonly CarcassDbContext _dbContext;
-
-    public CarcassUnitOfWork(CarcassDbContext dbContext)
-    {
-        _dbContext = dbContext;
-    }
-
-    public Task SaveChangesAsync(CancellationToken cancellationToken = default)
-    {
-        return _dbContext.SaveChangesAsync(cancellationToken);
-    }
-
-    public string GetTableName<T>() where T : class
-    {
-        var entType = _dbContext.Model.GetEntityTypes().SingleOrDefault(s => s.ClrType == typeof(T));
-        return entType?.GetTableName() ?? throw new Exception($"Table Name is null for {typeof(T).Name}");
-    }
-
-    public Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default)
-    {
-        return _dbContext.Database.BeginTransactionAsync(cancellationToken);
-    }
-
-    public async Task<Option<Err[]>> ExecuteSqlRawRetOptionAsync(string sql,
-        CancellationToken cancellationToken = default)
-    {
-        try
-        {
-            await _dbContext.Database.ExecuteSqlRawAsync(sql, cancellationToken);
-            return null;
-        }
-        catch (Exception e)
-        {
-            return new[] { SystemToolsErrors.UnexpectedDatabaseException(e) };
-        }
-    }
-
-    public void SetCommandTimeout(TimeSpan timeout)
-    {
-        _dbContext.Database.SetCommandTimeout(timeout);
-    }
-}
+public /*open*/ class CarcassUnitOfWork(CarcassDbContext ctx) : UnitOfWork(ctx);
