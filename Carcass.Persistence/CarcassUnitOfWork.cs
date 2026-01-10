3787f956a9a6b8ca0e69e366ae517d8700640226
merabza <merabza@gmail.com>
2026-01-10 20:33:33 +0400

    202601102033

---
+++ Carcass.Persistence/CarcassUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using Carcass.Database;
+using RepositoriesShared;
+
+namespace Carcass.Persistence;
+
+public class CarcassUnitOfWork : UnitOfWork
+{
+    public CarcassUnitOfWork(CarcassDbContext dbContext) : base(dbContext)
+    {
+    }
+}
