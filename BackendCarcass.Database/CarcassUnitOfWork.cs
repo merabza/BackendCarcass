174c1d03e53256eda55b89717621e8cec7d81523
merabza <merabza@gmail.com>
2026-01-28 21:26:29 +0400

    202601282126

---
+++ BackendCarcass.Database/CarcassUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using SystemTools.RepositoriesShared;
+
+namespace BackendCarcass.Database;
+
+public /*open*/ class CarcassUnitOfWork : UnitOfWork
+{
+    protected CarcassUnitOfWork(CarcassDbContext dbContext) : base(dbContext)
+    {
+    }
+}
